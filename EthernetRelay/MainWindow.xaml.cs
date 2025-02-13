48c31e845b9214ba3df9d054fea603bf42dc742b
Nikag0 <Serito2@mail.ru>
2025-02-13 11:50:15 +0300

    Connetction

---
+++ EthernetRelay/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Text;
+
+namespace EthernetRelay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            string message = "Hello METANIT.COM";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("192.168.1.203"), 1200);
+            int bytes = await udpSocket.SendToAsync(data, remotePoint);
+        }
+    }
+}
---
+++ EthernetRelay/RelayManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace EthernetRelay
+{
+    public class RelayManager
+    {
+        UdpClient udpc = new UdpClient(1200);
+        IPEndPoint localPoint = new IPEndPoint(IPAddress.Parse("192.168.1.203"), 1200);
+    }
+}
